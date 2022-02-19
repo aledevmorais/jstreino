eb8a570e7c56c2c2726b01b21f1ea195db3a46be
aledevmorais <alessandro@controllermorais.com.br>
2022-02-19 10:34:58 -0300

    escrita de variaveis

---
+++ type-strings2.js
@@ -0,0 +1,18 @@
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+// JavaScript é case-sensitive, ou seja, diferencia maiúsculas e minúsculas
+// came1case: minhaVar ou outraVar -> comeca com minúscula e a proxima palavra maiúscula
+// este cama1case -> é convenção utilizada pelo JS para vars e functions
+// snake-case -> (_)underline para vars and functions ex: minha_var e outra_var
+// kebab-case ->(-)hífens para vars and functions ex: minha-var E outra-var "NAÕ USAR, VAI DAR ERRO!!!!"
+// PascalCase -> Primeira letra Maiúscula para vars and functions  ex: Minhavar OutraVar
+// Variáveis NÃO COMEÇAM COM NUMEROS E NEM CARACTERES ESPECIAIS
+
+
+
