14ebff784d35ed0e4ee4de6d6c392249e85ced5c
aledevmorais <alessandro@controllermorais.com.br>
2022-02-22 10:01:31 -0300

    operador

---
+++ type-number2.js
@@ -0,0 +1,9 @@
+// salvar em uma var o resultado de uma operação
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) * 10;
+
+console.log(operacao)
+
+// o que está parenteses é executado primeiro
