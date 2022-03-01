04eb429d3b7d7769c589340d20095031b346ef36
aledevmorais <alessandro@controllermorais.com.br>
2022-03-01 09:50:50 -0300

    bug classico

---
+++ truthy-falsy.js
@@ -0,0 +1,34 @@
+// boolear
+
+const usuarioLogado = true
+const contapaga = false
+
+// 0, 1 em algumas linguagens são utilizados como false or true
+
+// no js o 0 -> é false e o 1 -> é true
+
+console.log(0 == false)
+console.log ("" == false)
+
+// return true pois o js ja identica a logica acima
+
+//indefined -> indefinido
+//null -> nulo, vazio, nada..
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura"
+
+//typeof qual é o tipo do dado guardado
+
+console.log(typeof minhVar);
+console.log(typeof varNull);
+console.log(typeof numero);
+console.log(typeof texto);
+
+// return object e não null bug de linguagem que não sera consertado
