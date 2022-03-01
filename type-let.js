6662b8fb80d7b872883fa9fc5bb870b50b0cb242
aledevmorais <alessandro@controllermorais.com.br>
2022-03-01 09:04:12 -0300

    let , var ou const

--- type-let.js
+++ type-let.js
@@ -38,4 +38,27 @@ console.log(area3);
 // ele buscou no if se forma for um retangulo usar a instrução contida em else
 // calculando assim a area do retangulo
 
+// tudo que é feito entre chaves é um bloco 
+
+const forma4 = "quadrada";
+const altura4 = 8;
+const comprimento4 = 6;
+//const area4; 
+let area4;
+
+if (forma4 === "quadrada") {
+    area4 = altura4 * comprimento4;
+}   else {
+    (area4 = altura * comprimento4) /2
+}
+console.log(area4)
+// variavel constante tem que ter um valor atribuido
+// no exemplo acima que dara erro pois a const exige um valor atribuido 
+// nesse caso tem que usar uma let
+// var não é recomendado usar por ser muito solta
+// VAR NÃO USA MAIS POR SER MUITO PROPENSA A BUGS
+
+
+
+
 
