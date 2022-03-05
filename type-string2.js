7a134166a7ab590071d0422c58c9a48c476245d6
aledevmorais <alessandro@controllermorais.com.br>
2022-03-05 10:51:50 -0300

    String2 more samples

---
+++ type-string2.js
@@ -0,0 +1,41 @@
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone));
+// no caso acima é uma concatenação de numero + string atribuida
+
+let telefone2 = "(11) 93440400";
+
+console.log("O Número do Telefone é: " + telefone2);
+
+// sample to preencher o  numero do telefone
+
+let ddd3 = 11;
+let telefone3 = 999010002
+
+console.log("Conforme solicitado o tel é :" + " " + String(ddd3) + " " + String(telefone3));
+
+// to string outra forma de transformação mais parecido com escrita do c#
+
+let ddd4 = 11;
+let telefone4 = 999010003
+
+console.log("Exemplo de to string conversão :" + " " + ddd4.toString() + " " + telefone4.toString());
+
+// converter bollean to string
+
+let usuarioconectado = false;
+
+console.log(String(usuarioconectado));
+
+// return false por causa da conversão em string
+
+let usuarioconectado1 = true
+
+console.log(usuarioconectado1.toString());
+
+// return true por causa da conversão em string
+
+
+
+
+
+
