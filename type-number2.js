a8b038bda306c6f06efce56006f70cb72d015c95
aledevmorais <alessandro@controllermorais.com.br>
2022-03-05 11:00:50 -0300

    Conversões Number explícitas

---
+++ type-number2.js
@@ -0,0 +1,10 @@
+// conversão de textos e booleanos atraves do number
+
+let largura = "10";
+let altura = "5";
+
+console.log(Number(largura) * Number(altura));
+
+// pode multiplicar, somar, dividir estrair depois da atribuição
+
+// utilizar no js conversões de forma explicita
