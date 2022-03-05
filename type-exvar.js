f8b5fb839ad079526b97f5e720736d304fbdb24a
aledevmorais <alessandro@controllermorais.com.br>
2022-03-05 11:38:46 -0300

    Outros bugs var

---
+++ type-exvar.js
@@ -0,0 +1,19 @@
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
+
+// a var altera o valor enquanto que a e let e a const não alteram
+
+//const não pode ser alterado o valor dela
+
+// O JavaScript converte automaticamente number for text for boolean
+// porem não podemos deixar isso acontecer automaticamente, pois gera bugs
