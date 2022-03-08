0ad8b685b7f372f66a09c4ade0bfb36964169010
aledevmorais <alessandro@controllermorais.com.br>
2022-03-07 22:04:49 -0300

    erros e suas tratativas

---
+++ erros.js
@@ -0,0 +1,9 @@
+const numero = 0;
+
+// syntaxError -> erro de sintaxe
+// toda const implica um valor
+
+//stack trace cominho do codigo que executou
+// RangError -> só aceita numeros inteiros
+// ReferenceError -> variavel não definada
+// TypeError -> tipo definido não foi retornado ex.: number ao inves de string conforme declaração
