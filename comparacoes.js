b45fb92938946c593524af9a57dbd4454ecf1197
aledevmorais <alessandro@controllermorais.com.br>
2022-03-19 19:03:49 -0300

    igual 2 vezes e 3 vezes

---
+++ comparacoes.js
@@ -0,0 +1,26 @@
+// == comparação implicita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// return true -> mesmo sendo string
+
+// === comparação explicita
+
+const numero1 = 5;
+const texto1 = "5";
+
+console.log(numero1 === texto1);
+
+// para identificar os tipos de dados que a variavel esta armazenando
+
+console.log(typeof numero1);
+console.log(typeof texto1);
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+// so usar os === como boas praticas
+
