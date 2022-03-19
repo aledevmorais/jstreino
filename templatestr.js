ccbb0e1586efb5e51f676b4f683ba98322ab3a5b
aledevmorais <alessandro@controllermorais.com.br>
2022-03-19 20:03:15 -0300

    Tamplate String Samples

---
+++ templatestr.js
@@ -0,0 +1,27 @@
+const nome = "Ale";
+const apresentacao = "Meu nome é " + nome;
+
+console.log(apresentacao);
+
+// concatenação string é acima
+
+const nome1 = "Alessandro";
+const idade = 2022-1977;
+const cidadenasc = "Osasco"
+
+const meusdados = "Meu nome é " + nome1 + ", minha idade é : " + idade + ", e nasci na cidade de : " +
+cidadenasc;
+
+console.log(meusdados);
+
+const nome2 = "Alessandro";
+const idade1 = 2022-1977;
+const cidadenasc1 = "Osasco"
+
+// sinal de crase " ` " template string abaixo
+
+const meusdados1 = `Meu nome é ${nome2} e tenho a idade de ${idade1} e moro na cidade de ${cidadenasc1}`;
+
+console.log(meusdados1);
+
+// este é um caso de tamplete string $ entre chaves
