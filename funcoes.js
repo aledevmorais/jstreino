8165aed7d0c335304adfdd2cdf4b00c7a689c4e8
aledevmorais <alessandro@controllermorais.com.br>
2022-03-20 20:12:30 -0300

    Funções varios modelos

--- funcoes.js
+++ funcoes.js
@@ -33,9 +33,42 @@ console.log(soma());
 
 // return ele fecha e armazena
 
+const numero = Math.round(4.3);
+
+console.log(numero);
+
+// irá arredondar para 4
+
+const numero1 = Math.round(3.85);
+
+console.log(numero1);
+
+// experimentar 3,85 -> 3 e 3.85 -> 4
+
+const numero2 = Math.round(2.5);
+
+console.log(numero2);
+
+// return 3 pois irá arrendondar pra cima
+// 0.5 a função arredonda para cima
+
+// exemplos de match só declarar uma const e fazer os calculos
+
+// Math.ceil(5.2) retorna 6 -> faz o arredondamento para o mais up
+// Math.floor(5.2) retorna 5 -> arrendondar mais down
+// Math.trunc(4.3) retorna 4 -> desconsidera os decimais
+// Math.trunc(4.8) retorna 4 -> não importa os decimais vai  considerar somente o inteiro
+// Math.pow(4 , 2) retorna 4^2 = 16 -> exponenciação
+// Math.sqrt(64) retorna 8 -> raiz quadrada
+// Math.min(0, 150, 30, 20, -8, -200) retorna -200 -> retorna o menor valor entre os argumentos e numeros
+// Math.max(0, 150, 30, 20, -8, -200) retorna 150 - > return the max value
+
+// exemplo math randomicos - escolher qualquer value de forma aleatória
 
 
+const numero3 = Math.random(2, 5, 19, 38, 21, 12, 8, 17, 41, 26);
 
+console.log(numero3);
 
-
+// ira retornar qualquer valor aleatório não importa cadeia de dados
 
