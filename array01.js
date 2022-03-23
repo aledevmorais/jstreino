c87a0e257aaea73524cfd4bac198403e70f2fe3f
aledevmorais <alessandro@controllermorais.com.br>
2022-03-23 10:30:48 -0300

    Array propriedades

---
+++ array01.js
@@ -0,0 +1,33 @@
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array
+const numeros = [50, 43, 12];
+
+// variavel guarda apenas um dado e array guarda uma lista de dados
+
+// array -> Pode ser usada, por exemplo, para agrupar diversos dados que têm relação entre si.
+
+
+const arrayDeNumeros = [50, 43, 12];
+const arrayDeStrings = ["banana", "alura", "Juliana"];
+
+// cada valor é chamado de elemento
+
+// array com 1 elemento
+const numero = [5];
+
+// array com 3 elementos
+const nums1 = [50, 43, 12];
+
+// propriedade -> length 
+
+// índice     0   1   2   3
+const nums2 = [50, 43, 12, 98];
+console.log(nums2.length) 
+
+// return 4 elementos
+
+
+
