7aa17bff2dca1f8c38fba9aab98fb612397bf5d5
aledevmorais <alessandro@controllermorais.com.br>
2022-03-25 07:39:14 -0300

    Adicionar item a array

---
+++ nota.js
@@ -0,0 +1,12 @@
+const notas = [10, 6, 8]
+notas.push (7);
+
+console.log(notas);
+// return [10, 6, 8, 7]
+// método push ele adiciona um item a array
+
+let media = (notas [0] + notas [1] + notas [2] + notas [3])/ notas.length;
+
+console.log(media);
+// retorna média -> método de array que recebe um parametro
+// o push adiciona ao ultimo ítem da array
