ba0e211245d568753e523b20b0456c4db87552eb
aledevmorais <alessandro@controllermorais.com.br>
2022-04-03 13:24:08 -0300

    Add campos no objeto

---
+++ adicionarcampos.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Alessandro",
+    idade:44,
+    cpf: "13113213334",
+    email: "alessandro@controllermorais.com.br",
+}
+console.log(cliente);
+// return a lista acima sem o telefone
+
+cliente.fone = "01199990000"
+// no objeto é mais facil adicionar outro item a lista diferente do array
+console.log(cliente);
+
+// return a mesma lista adicionando o telefone
