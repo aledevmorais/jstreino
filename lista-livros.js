8575a7edf041155f14b69b83e9c7906d9618850a
aledevmorais <alessandro@controllermorais.com.br>
2022-04-11 12:44:58 -0300

    Array -> Lista para usada em outro código preco-livros.js

---
+++ lista-livros.js
@@ -0,0 +1,30 @@
+const precoLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25,
+    },
+    {
+        titulo: "JavaScript II",
+        preco: 15,
+    },
+    {
+        titulo: "JavaScript III",
+        preco: 30,
+    },
+    {
+        titulo: "JavaScript IV",
+        preco: 50,
+    },
+    {
+        titulo: "JavaScript V",
+        preco: 45,
+    },
+
+    {
+        titulo: "JavaScript VI",
+        preco: 20,
+    }];
+
+module.exports = precoLivros;
+
+
